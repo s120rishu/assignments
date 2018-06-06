46f6816a9a2ed0d093d8fe26618b9fd7393be913
s120rishu <sumit110dhiman@gmail.com>
2018-06-06 23:54:38 +0530

    assignment2 file

---
+++ assignment2.py
@@ -0,0 +1,28 @@
+#Print anything
+print ("hello class")
+
+#Join two String
+print("acad"+"view")
+
+#Print value of three variable
+age=22
+pay=30000.00
+Name="sumit"
+print (Name,age,pay)
+
+
+#print let's get started on screen
+print('let\'s get started')
+
+
+#print the values using place holders
+s="acadview"
+course="python"
+fees=5000
+print(('%s''%s''%d')%(s,course,fees)
+
+#something intresting exsesise
+
+name='Tony stark'
+Salary=1000000
+print(('%s''%d')%(name, Salary))
