2ac6b3f7dbdebdd9b2d3ce1518d5675e3fd8839b
s120rishu <sumit110dhiman@gmail.com>
2018-06-07 12:14:09 +0530

    assignment3

---
+++ assigment3.py
@@ -0,0 +1,38 @@
+#Q1 create a list with user defined inputs
+a=[]
+b=input("enter user define list")
+a.append(b)
+print(a)
+
+
+#Q2
+
+c =['apple','google','facebook','microsoft','tesla','google']
+a.append(c)
+print(a)
+
+#Q3
+print(c.count('google'))
+print(c.count("apple"))
+
+#Q4
+d=[85,96,85,115,2,3,45]
+d.sort()
+print(d)
+
+#Q5
+e=[95,54,77,2,9,7,8]
+e.sort()
+print(e)
+f=d+e
+f.sort()
+print(f)
+
+
+#Q6
+
+stack=['appen','pop','data']
+stack.append('roll')
+print(stack)
+stack.pop()
+print(stack)
